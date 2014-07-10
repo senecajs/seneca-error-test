99e775c929ce49c713e126724e517ee8ee92ecb8
Richard Rodger <richard@ricebridge.com>
2014-07-10 17:08:00 +0100

    moved from seneca, bump 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-seneca-error-test
-=================
+# seneca-error-test
 
-Seneca error test.
+### Seneca error test.
+
+
+### Support
+
+If you're using this module, feel free to contact me on twitter if you
+have any questions! :) [@rjrodger](http://twitter.com/rjrodger)
+
+Current Version: 0.1.0
+
+Tested on: Node 0.10.29, Seneca 0.5.19
+
+
+
+
+
---
+++ error-test.js
@@ -0,0 +1,40 @@
+/* Copyright (c) 2012-2014 Richard Rodger */
+
+"use strict";
+
+
+module.exports = function( options ) {
+
+  this.add({role:'error-test'},function(args,cb){
+    var seneca = this
+
+    if( 'fail' == args.how ) {
+      throw seneca.fail('error_code1')
+    }
+    else if( 'errobj' == args.how ) {
+      throw new Error('an Error object')
+    }
+    else if( 'str' == args.how ) {
+      throw('a string error')
+    }
+    else if( 'obj' == args.how ) {
+      throw({bad:1})
+    }
+    else if( 'cb-err' == args.how ) {
+      return cb(new Error('cb-err'))
+    }
+    else if( 'cb-fail' == args.how ) {
+      return cb(seneca.fail('cb-fail'))
+    }
+    else if( 'cb-obj' == args.how ) {
+      return cb({bad:2})
+    }
+    else if( 'cb-cb-err' == args.how ) {
+      return cb(new Error('cb-cb-err'))
+    }
+    else cb()
+  })
+
+  return {name:'error-test'}
+}
+
